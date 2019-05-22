1b6ff59c49d75b9131708485846d4c86fafe0950
hikeuchi <hikeuchi@morisawa.co.jp>
2019-05-22 15:46:40 +0900

    initial commit

---
+++ ChangeableHeightTextViewSample/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ChangeableHeightTextViewSample
+//
+//  Created by HIROKI IKEUCHI on 2019/05/22.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
